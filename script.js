97cd792f7eab1f1f8969bac0960ca17ca4415709
xPrinZ_ <71972438+FoxyCodes234@users.noreply.github.com>
2024-02-29 18:19:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+function addQuestion() {
+  const formContainer = document.getElementById("formContainer");
+  const newContainer = document.createElement("div");
+  const container = document.getElementById("container");
+  const newH3 = document.createElement("h3");
+  const newTextBox = document.createElement("input");
+  const newButton = document.createElement("button");
+  newH3.innerText = "What is your name?";
+  newTextBox.setAttribute.type = "text";
+  newTextBox.placeholder = "Enter Name Here";
+  newTextBox.setAttribute("id", "textBox");
+  newButton.innerText = "Submit";
+  newButton.onclick = function checkName() {
+    if (document.getElementById("textBox").value != "") {
+      const textBox = document.getElementById("textBox").value;
+      newContainer.remove();
+      newH3.innerText = `Welcome, ${textBox}`;
+      container.appendChild(newH3);
+    }
+  };
+
+  newContainer.appendChild(newH3);
+  newContainer.appendChild(newTextBox);
+  newContainer.appendChild(newButton);
+  container.appendChild(newContainer);
+}
+
+function buttonClick() {
+  const formContainer = document.getElementById("formContainer");
+  const userName = document.getElementById("username");
+  const passowrd = document.getElementById("password");
+  const fLabel = document.getElementById("fLabel");
+  const sLabel = document.getElementById("sLabel");
+  if (userName.value.toLowerCase() == "admin" && passowrd.value == "123") {
+    formContainer.remove();
+    addQuestion();
+  } else if (
+    userName.value.toLowerCase() == "admin" &&
+    passowrd.value != "123"
+  ) {
+    sLabel.style.color = "red";
+  } else if (
+    userName.value.toLowerCase() != "admin" &&
+    passowrd.value == "123"
+  ) {
+    fLabel.style.color = "red";
+  } else {
+    fLabel.style.color = "red";
+    sLabel.style.color = "red";
+  }
+  userName.value = "";
+  passowrd.value = "";
+}
